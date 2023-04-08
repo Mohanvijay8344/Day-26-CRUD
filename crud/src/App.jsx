3bdd64d12c3d3aa918ecf4aec8cda2ffac48faad
Mohanraj <Mohanvijay8344@gmail.com>
2023-04-08 21:18:58 +0530

    Day-26-CRUD

---
+++ crud/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import DrawerAppBar from './components/navbar';
+import CreateUser from './components/form';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Users  from './components/users'
+import Userdetails  from './components/userdetail'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <ToastContainer theme='dark'/>
+         < DrawerAppBar/>
+         <Routes>
+        
+          <Route path="/" element={<Users />} />
+          <Route path="/Userdetails/:id" element={<Userdetails />} />
+          <Route path="/CreateUser" element={ <CreateUser />} />
+          <Route path="/CreateUser/:id" element={ <CreateUser />} />
+        </Routes>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
